7a86865f1603e2cc9f0217b36e3b86ad3dccb6af
minciu <stefan_minciuna@yahoo.com>
2020-04-27 11:17:43 +0300

    Create ChartsRepository.java

---
+++ lab9/src/main/java/repo/ChartsRepository.java
@@ -0,0 +1,37 @@
+package repo;
+
+import clojure.lang.Compiler;
+import entity.Charts;
+import javax.persistence.*;
+import entity.Album;
+import entity.Artist;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChartsRepository extends GenericRepository{
+    private EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("MusicAlbumsPU");
+    EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+    
+    public List<Charts> sortAlbumsCharts(){
+        List<Charts> sortedAlbums = new ArrayList<>();
+        String sortingQuery = "SELECT c.id, c.album_id FROM Chart c JOIN Album a ON c.album_id = a.id ORDER BY a.release_year";
+        Query query = entityManager.createQuery(sortingQuery);
+        sortedAlbums = query.getResultList();
+        return sortedAlbums;
+    }
+
+
+    /**
+     * Returns a list of charts representing albums that match a genre given as parameter
+     * @param musicGenre
+     * @return
+     */
+    public List<Charts> getByGenres(String musicGenre){
+        List<Charts> foundAlbums = new ArrayList<>();
+        String sortingQuery = "SELECT c.id, c.album_id FROM Char c JOIN Album a ON c.album_id = a.id WHERE a.musicGenre = musicGenre";
+        Query query = entityManager.createQuery(sortingQuery);
+        foundAlbums = query.getResultList();
+        return foundAlbums;
+    }
+}
